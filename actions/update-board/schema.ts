68f7c086b709c9130fd8ab7c56d18c76d0c9dcff
Luciano Arias <lucianoarias1997@gmail.com>
2024-04-25 22:17:39 -0300

    board page update and delete

---
+++ actions/update-board/schema.ts
@@ -0,0 +1,13 @@
+import { z } from 'zod';
+
+export const UpdateBoard = z.object({
+  title: z
+    .string({
+      required_error: 'El titulo es requerido',
+      invalid_type_error: 'El titulo es requerido',
+    })
+    .min(3, {
+      message: 'El titulo es muy corto',
+    }),
+  id: z.string(),
+});
