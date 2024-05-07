df2c685af01d1339a0b29088734321a0af96fa52
Luciano Arias <lucianoarias1997@gmail.com>
2024-05-07 00:07:19 -0300

    add card modal

---
+++ actions/update-card/schema.ts
@@ -0,0 +1,24 @@
+import { z } from 'zod';
+
+export const UpdateCard = z.object({
+  boardId: z.string(),
+  description: z.optional(
+    z
+      .string({
+        required_error: 'La descripción es requerida',
+        invalid_type_error: 'La descripción es requerida',
+      })
+      .min(3, {
+        message: 'La descripción es muy corta',
+      })
+  ),
+  title: z
+    .string({
+      required_error: 'El título es requerido',
+      invalid_type_error: 'El título es requerido',
+    })
+    .min(3, {
+      message: 'El título es muy corto',
+    }),
+  id: z.string(),
+});
